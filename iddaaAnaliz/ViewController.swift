a87cc42722b4ee17ff0b6fe76c93b62746513326
Umut TOKER <umt.toker@gmail.com>
2016-04-04 06:36:18 -0700

    Initial Commit

---
+++ iddaaAnaliz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iddaaAnaliz
+//
+//  Created by umut toker on 04/04/16.
+//  Copyright © 2016 aucorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
