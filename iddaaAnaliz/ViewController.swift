62aba303cbe75fe874618bac92f8c76d951e1fa0
Umut TOKER <umt.toker@gmail.com>
2016-04-07 23:57:23 +0300

    tutorial step 2 ok. some very basic steps happened

--- iddaaAnaliz/ViewController.swift
+++ iddaaAnaliz/ViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Properties
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    // MARK: Actions
+    @IBAction func changeButtonAction(sender: UIButton) {
+        mealNameLabel.text = "This is the Default texts"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameTextField.delegate = self
     }
 
 
+    // MARK: UITextFieldDelegate
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // this gonna hide keyboard
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
 }
 
